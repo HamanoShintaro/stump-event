3fab85cb354ae978dd99d1b5a0fed31f29612bfe
ShintaroHamano <shintaro.action@gmail.com>
2026-05-17 10:24:59 +0900

    fix: remove new keyword from Leaflet icon factory functions to fix build error

--- src/components/MapUI.tsx
+++ src/components/MapUI.tsx
@@ -16,7 +16,7 @@ L.Icon.Default.mergeOptions({
 });
 
 // カスタムアイコン（現在地用）
-const UserLocationIcon = new L.Icon({
+const UserLocationIcon = L.icon({
   iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
   iconSize: [25, 41],
@@ -44,7 +44,7 @@ const getCategoryIcon = (category: string) => {
     default: emoji = "🚩"; bgColor = "#D9655B"; break;
   }
 
-  return new L.divIcon({
+  return L.divIcon({
     html: `
       <div style="
         background-color: white; 
@@ -77,7 +77,7 @@ const getCategoryIcon = (category: string) => {
 };
 
 // カスタムアイコン（スポット）
-const SpotIcon = new L.divIcon({
+const SpotIcon = L.divIcon({
   html: '<div style="font-size: 24px; filter: drop-shadow(0 2px 4px rgba(0,0,0,0.3)); text-align: center; line-height: 1; margin-top: -12px; margin-left: -12px;">📍</div>',
   className: 'spot-icon',
   iconSize: [0, 0], // CSS overrides
